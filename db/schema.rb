04b5e75c64a519a6c5253d11932b412b1c945747
Daniel Jacob Archer <me@danieljacobarcher.com>
2013-11-11 17:29:48 -0800

    New Seeds and Migration

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131112010923) do
+ActiveRecord::Schema.define(version: 20131112012540) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "users", force: true do |t|
+    t.string   "name"
+    t.string   "city"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
